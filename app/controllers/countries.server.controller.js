16f6f3520be0abb56a81772dbbc6b540610b3f20
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berkbuyukdurak07@gmail.com>
2022-01-08 18:38:04 +0300

    Functionality is added for returning all countries with the "region=all" query. Also, a new function is implemented for handling reponses.

--- app/controllers/countries.server.controller.js
+++ app/controllers/countries.server.controller.js
@@ -2,10 +2,13 @@ const logger = require('../utils/logger/logger');
 const apiResponse = require('../utils/api_response');
 
 // Countries Service
-const countriesSerivce = require('../services/countries.service');
+const countriesService = require('../services/countries.service');
 
 // Response Codes and Messages
 const responseCodesAndMessages = require('../utils/constants/http_response_status_codes_and_messages.json');
+
+// Lodash
+const _ = require('lodash');
 
 const getCountriesByRegion = async(req, res, next) => {
     let payloadToSent = {};
@@ -15,28 +18,41 @@ const getCountriesByRegion = async(req, res, next) => {
         payloadToSent = req.body.region;
     }
     try{
-        countriesSerivce.findCountriesWithTheirRegion(payloadToSent).then(returnedData => {
-            let responsePayload = {};
-            if(returnedData.length > 0){
-                responsePayload.statusCode = responseCodesAndMessages.ok.statusCode;
-                responsePayload.data = returnedData;
-                const successMessage = responseCodesAndMessages.ok.message +  " Data is successfully found and returned.";
-                const returnSuccess = `${successMessage} ${responsePayload.statusCode}`;
-                logger.info(returnSuccess);
-            }else{
-                responsePayload.statusCode = responseCodesAndMessages.not_found.statusCode;
-                responsePayload.message = responseCodesAndMessages.not_found.message;
-                const returnNotFound = `${responsePayload.message} ${responsePayload.statusCode}`;
-                logger.error(returnNotFound);
-            }
-            res.status(responsePayload.statusCode).send(responsePayload);
-
-
-        });
+        if(!_.isEqual(_.lowerCase(payloadToSent), "all")){
+            countriesService.findCountriesWithTheirRegion(payloadToSent).then(returnedData => {
+                let responseToSent = preparePayload(returnedData);
+                res.status(responseToSent.statusCode).send(responseToSent);    
+            });
+        }else{
+            countriesService.findAllCountries().then(returnedData => {
+                let responseToSent = preparePayload(returnedData);
+                res.status(responseToSent.statusCode).send(responseToSent);   
+            });
+        }
     }
     catch(err){
         console.log(err)
     }
+}
+
+/*
+* Prepare Payload Function to Return Data
+*/
+const preparePayload = (returnedData) => {
+    let responsePayload = {};
+    if(returnedData.length > 0){
+        responsePayload.statusCode = responseCodesAndMessages.ok.statusCode;
+        responsePayload.data = returnedData;
+        const successMessage = responseCodesAndMessages.ok.message +  " Data is successfully found and returned.";
+        const returnSuccess = `${successMessage} ${responsePayload.statusCode}`;
+        logger.info(returnSuccess);
+    }else{
+        responsePayload.statusCode = responseCodesAndMessages.not_found.statusCode;
+        responsePayload.message = responseCodesAndMessages.not_found.message;
+        const returnNotFound = `${responsePayload.message} ${responsePayload.statusCode}`;
+        logger.error(returnNotFound);
+    }
+    return responsePayload;
 }
 
 
